673874488004b64b1a5849765d623627d9da5a8a
AlisonDev-Web <alisonsoares223@gmail.com>
2024-09-17 11:45:44 -0300

    Add files via upload
    Site por enquanto incompleto, com o objetivo de estruturar e obter conhecimento sobre matérias e áreas diversas do estudo !

---
+++ Imersão Alura/app.js
@@ -0,0 +1,13 @@
+function Pesquisar(){
+
+let section = document.getElementById("resultados-pesquisa")
+console.log(section);
+
+section.innerHTML = `<div class="item-resultado">
+<h1>Matemática: A Linguagem dos Números e das Formas</h1>
+<p class="descricao-meta">A Matemática é uma ciência que estuda padrões, quantidades, espaços, estruturas e mudanças. É a linguagem universal que utilizamos para descrever o mundo ao nosso redor, desde os fenômenos mais simples até os mais complexos.</p>        
+<A href="https://www.todamateria.com.br/matematica/" target="_blank"> <ins>Para mais informações e exercicios relacionados</ins></A>
+</div>`
+
+}
+console.log(dados)
---
+++ Imersão Alura/dados.js
@@ -0,0 +1,31 @@
+let dados = [
+    { 
+        titulo: "Matemática: A Linguagem dos Números e das Formas",
+        descricao :"A Matemática é uma ciência que estuda padrões, quantidades, espaços, estruturas e mudanças. É a linguagem universal que utilizamos para descrever o mundo ao nosso redor, desde os fenômenos mais simples até os mais complexos.",
+        link:"https://www.todamateria.com.br/matematica/"
+    
+    
+    },
+    {
+       titulo: "A Língua Portuguesa: Um Mundo de Palavras",
+        descricao: "A Língua Portuguesa é muito mais do que apenas um meio de comunicação. É um universo rico e complexo que nos permite expressar ideias, sentimentos e construir relações.",
+        link:"https://www.todamateria.com.br/lingua-portuguesa/"
+    
+    
+    },
+    {
+        titulo:"As Ciências da Natureza: Desvendando os Mistérios do Universo" ,
+        descricao: "As Ciências da Natureza são a porta de entrada para a compreensão do mundo físico que nos cerca. Através delas, exploramos os fenômenos naturais, desde a constituição da matéria até os complexos processos da vida.",
+        link:"https://www.todamateria.com.br/ciencias-da-natureza-e-suas-tecnologias/"  
+        
+    
+    },
+    {
+        titulo: "As Ciências Humanas: Um Olhar para o Ser Humano e a Sociedade",
+        descricao: "As Ciências Humanas são um conjunto de disciplinas que se dedicam a estudar o ser humano, suas relações sociais, culturais e históricas.",
+        link:"https://www.todamateria.com.br/enem/ciencias-humanas-e-tecnologias/"  
+    
+    
+    }
+];
+
